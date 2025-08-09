365f420c407f36b0ebab29764ea05f717bb9cf9a
Rizki Cahya <rizki.cahya03@gmail.com>
2025-08-09 15:58:43 +0700

    feat: delete 1 project on projects page

--- src/pages/projectsData.jsx
+++ src/pages/projectsData.jsx
@@ -7,45 +7,38 @@ export const projects = [
         delay: 0
     },
     {
-        title: "Public Complaint System",
-        description: "A web-based public complaint system using PHP and MySQL.",
-        tech: ["PHP", "MySQL", "Bootstrap"],
-        link: "https://github.com/rizkicahya-ramdani/aplikasi-pengaduan-masyarakat",
-        delay: 100
-    },
-    {
         title: "Blog System",
         description: "A blog creation system with a responsive layout using PHP, MySQL, Tailwind CSS, and Docker",
         tech: ["PHP", "MySQL", "Tailwind CSS", "Docker"],
         link: "https://github.com/rizkicahya-ramdani/ClaireBlog",
-        delay: 200
+        delay: 100
     },
     {
         title: "Hotel Ticket Booking System",
         description: "A web-based hotel ticket booking system using CodeIgniter and MySQL.",
         tech: ["PHP", "CodeIgniter", "Bootstrap", "MySQL"],
         link: "https://github.com/rizkicahya-ramdani/aplikasi-pemesanan-hotel",
-        delay: 300
+        delay: 200
     },
     {
         title: "Heart Disease Classification",
         description: "Heart disease classification using Random Forest and Logistic Regression algorithms.",
         tech: ["Python", "Jupyter"],
         link: "https://github.com/rizkicahya-ramdani/klasifikasi-penyakit-jantung",
-        delay: 400
+        delay: 300
     },
     {
         title: "Cinema Ticket Booking System",
         description: "A web-based cinema ticket booking system using PHP and MySQL.",
         tech: ["PHP", "MySQL", "Bootstrap"],
         link: "https://github.com/Fauzi-Taufiq/bioskop",
-        delay: 500
+        delay: 400
     },
     {
         title: "Hotel Ticket Booking System (UI/UX)",
         description: "A mobile-based hotel ticket booking system prototype using Figma.",
         tech: ["Figma"],
         link: "https://www.figma.com/design/eSa9brf8IkddzMcMuTHBG2/Sistem-Pemesanan-Tiket-Hotel?node-id=0-1&t=K0ksMtmmtNOVZMN9-1",
-        delay: 600
+        delay: 500
     }
 ];
